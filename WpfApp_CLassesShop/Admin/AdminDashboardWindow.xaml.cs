d901303a0db26f41945dc03e16160c741602484a
Bach Ngo <bachngo14042005@gmail.com>
2026-03-27 01:02:51 +0700

    lam lai admin rr a

---
+++ WpfApp_CLassesShop/Admin/AdminDashboardWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp_CLassesShop.Session;
+
+namespace WpfApp_CLassesShop.Admin
+{
+    public partial class AdminDashboardWindow : Window
+    {
+        public AdminDashboardWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnManageUsers_Click(object sender, RoutedEventArgs e)
+        {
+            new AdminUsersWindow().ShowDialog();
+        }
+
+        private void BtnManageProducts_Click(object sender, RoutedEventArgs e)
+        {
+            // new AdminProductsWindow().ShowDialog();
+        }
+
+        private void BtnManageVariants_Click(object sender, RoutedEventArgs e)
+        {
+            // new AdminVariantsWindow().ShowDialog();
+        }
+
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentSession.LoggedInAccount = null;
+            new LoginWindow().Show();
+            this.Close();
+        }
+    }
+}
--- WpfApp_CLassesShop/RegisterWindow.xaml.cs
+++ WpfApp_CLassesShop/RegisterWindow.xaml.cs
@@ -1,23 +1,10 @@
 ﻿using BLL.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WpfApp_CLassesShop
 {
-    /// <summary>
-    /// Interaction logic for RegisterWindow.xaml
-    /// </summary>
     public partial class RegisterWindow : Window
     {
         private readonly AccountService _accountService;
